aabbc109b3603fffa4eb940189d4743cd04c6480
fredandhiof <fredand@hiof.no>
2022-10-27 17:46:42 +0200

    feat: Add leap_year_calculator.py, leap_year_tests.py and requirements.txt

---
+++ leap_year_tests.py
@@ -0,0 +1,26 @@
+from leap_year_calculator import is_leap_year
+
+
+def test_year_divisible_by_4000_is_not_leap_year():
+    assert not is_leap_year(4000)
+    assert not is_leap_year(8000)
+
+
+def test_year_divisible_by_400_is_leap_year():
+    assert is_leap_year(400)
+    assert is_leap_year(1600)
+
+
+def test_year_divisible_by_100_is_not_leap_year():
+    assert not is_leap_year(100)
+    assert not is_leap_year(900)
+
+
+def test_year_divisible_by_4_is_leap_year():
+    assert is_leap_year(4)
+    assert is_leap_year(2016)
+
+
+def test_year_divisible_by_3_is_not_leap_year():
+    assert not is_leap_year(3)
+    assert not is_leap_year(18)
